5432e0ec1fee62755acea6fe44e2ab70e872fb73
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-06-13 09:21:24 +0200

    Only signed in user can create a project

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,9 +11,19 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
     /** @test */
+    public function only_authenticated_users_can_create_projects()
+    {
+        $attributes = factory('App\Project')->raw();
+ 
+        $this->post('/projects', $attributes)->assertRedirect('login');
+    }
+    
+    /** @test */
     public function a_user_can_create_a_project()
     {
         $this->withoutExceptionHandling();
+
+        $this->actingAs(factory('App\User')->create());
 
         $attributes = [
             'title' => $this->faker->sentence,
@@ -45,6 +55,8 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['title' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('title');
@@ -53,16 +65,10 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_description()
     {
+        $this->actingAs(factory('App\User')->create());
+
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-    }
-
-    /** @test */
-    public function a_project_requires_an_owner()
-    {
-        $attributes = factory('App\Project')->raw();
- 
-        $this->post('/projects', $attributes)->assertRedirect('login');
     }
 }
