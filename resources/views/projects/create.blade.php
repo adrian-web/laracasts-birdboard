aca6ad0149a1f832c8daf509981d0eb51acf29c6
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-06-14 10:05:21 +0200

    Frontend CSS Tailwind support

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,18 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('content')
 
-<body>
-    <form action="/projects" method="POST">
-        @csrf
-        <h1>Create a project</h1>
-    </form>
-</body>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
 
-</html>
+                <div class="card-body">
+                    <form method="POST" action="/projects">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="title" class="col-md-4 col-form-label text-md-right">Title</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="title" class="form-control" name="title" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+
+                            <div class="col-md-6">
+                                <textarea name="description" id="description" class="form-control" cols="30" rows="10"
+                                    required></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Create
+                                </button>
+                                <a href="/projects">Cancel</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,26 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('content')
 
-<body>
-    <h1>Birdboard</h1>
-    <ul>
-        @forelse($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">
-                    {{ $project->title }}
-                </a>
-            </li>
-        @empty
-            <li>No projects yet.</li>
-        @endforelse
-    </ul>
-</body>
+<div class="flex items-center mb-3">
+    <h1 class="mr-auto">Birdboard</h1>
+    <a href="/projects/create">Create new project</a>
+</div>
+<ul>
+    @forelse($projects as $project)
+        <li>
+            <a href="{{ $project->path() }}">
+                {{ $project->title }}
+            </a>
+        </li>
+    @empty
+        <li>No projects yet.</li>
+    @endforelse
+</ul>
 
-</html>
+@endsection
