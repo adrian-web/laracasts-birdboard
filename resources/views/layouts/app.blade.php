494aa63e44dabc245e03344beb323b6ae9cfeb83
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-06-15 17:02:23 +0200

    Further TailwindCSS improvements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,10 +26,11 @@
         <nav class="bg-white">
             <div class="container mx-auto">
                 <div class="flex justify-between items-center py-2">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-
+                    <h1>
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="/images/logo.svg" alt="Birdboard">
+                        </a>
+                    </h1>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
                         <!-- Right Side Of Navbar -->
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,13 +2,24 @@
 
 @section('content')
 
-<a href="/projects/create">Create new project</a>
+<header class="flex items-center mb-3 py-4">
+    <div class="flex justify-between w-full items-center">
+        <h2 class="text-gray-600 text-sm font-normal">My Projects</h2>
+        <a href="/projects/create" class="button">Create new project</a>
+    </div>
+</header>
 
-<div class="flex">
+<div class="lg:flex lg:flex-wrap -mx-3">
     @forelse($projects as $project)
-        <div class="bg-white mr-4 rounded shadow p-5 w-1/3" style="height: 200px;">
-            <h3 class="text-xl py-4">{{ $project->title }}</h3>
-            <div class="text-gray-500">{{ Str::limit($project->description, 100) }}</div>
+        <div class="lg:w-1/3 px-3 pb-6">
+            <div class="bg-white rounded-lg shadow p-5" style="height: 200px;">
+                <h3 class="text-xl py-4 -ml-5 border-l-4 border-blue-300 pl-4 mb-3">
+                    <a href="{{ $project->path() }}" class="text-black no-underline">
+                        {{ $project->title }}
+                    </a>
+                </h3>
+                <div class="text-gray-600">{{ Str::limit($project->description, 100) }}</div>
+            </div>
         </div>
     @empty
         <div>No projects yet.</div>
