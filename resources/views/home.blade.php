f4b95ce87abfa68efbdc048ad836d2c2f7cea42c
adrian-web <65187349+adrian-web@users.noreply.github.com>
2020-07-01 10:42:54 +0200

    Fix login/register CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,17 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     You are logged in!
+
+                    <br>
+
+                    Visit your projects page: <a href="/projects"
+                        class="hover:underline hover:text-gray-700">Projects</a>
                 </div>
             </div>
         </div>
